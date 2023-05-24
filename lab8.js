37f3ba76d7e983293f78fd6ad6a7f2e6bd4f5a99
Матвеева Светлана <staruk.007mail.ru>
2023-05-25 00:13:14 +0700

    Время

--- lab8.js
+++ lab8.js
@@ -42,3 +42,12 @@ function ohistka() {
     inputDate.value="";
     result.innerHTML="";
 }
+
+function showTime() {
+    let today = new Date();
+    let currentTime = today.toLocaleTimeString('ru-RU');
+    document.getElementById('time').innerHTML = currentTime;
+
+}
+
+setInterval(showTime, 1000);
